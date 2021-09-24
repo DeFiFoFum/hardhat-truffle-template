e22e3b09d0b30c5b5491234a49a3786df4ded58e
defifofum <defifofum0@gmail.com>
2021-09-24 17:05:41 -0400

    feat: Setup initial hardhat repo - Typescript support for tests and scripts - Mocha test integration tools

---
+++ test/Greeter.test.ts
@@ -0,0 +1,26 @@
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+import { expect } from "chai";
+
+describe("Greeter", function () {
+  let accounts: Signer[];
+
+  beforeEach(async function () {
+    accounts = await ethers.getSigners();
+  });
+
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
